ad963fe515ee679371b4a199a7e88b43e865a669
CerulanLumina <CerulanLumina@users.noreply.github.com>
2020-01-29 18:56:51 -0500

    Add outline

--- src/main/java/bettergraves/block/BetterGraveBlock.java
+++ src/main/java/bettergraves/block/BetterGraveBlock.java
@@ -6,11 +6,14 @@ import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.EntityContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
@@ -31,6 +34,15 @@ public class BetterGraveBlock extends Block implements BlockEntityProvider {
             world.removeBlock(pos, false);
             return ActionResult.SUCCESS;
         } else return ActionResult.FAIL;
+    }
+
+    private static VoxelShape plate = VoxelShapes.cuboid(0, 0, 0, 1.0, 0.125, 1.0);
+    private static VoxelShape head = VoxelShapes.cuboid(0.25, 0.0, 0.25, 0.75, 0.625, 0.75);
+    private static VoxelShape outline = VoxelShapes.union(plate, head);
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, EntityContext ePos) {
+        return outline;
     }
 
     @Override
