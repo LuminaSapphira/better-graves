e071e4dc61a823d2bfc6795b9aca46fe7a054ecb
CerulanLumina <CerulanLumina@users.noreply.github.com>
2020-02-01 13:33:08 -0500

    Add logs and crash on duplicate key to API

--- src/main/java/bettergraves/api/BetterGravesAPI.java
+++ src/main/java/bettergraves/api/BetterGravesAPI.java
@@ -1,5 +1,10 @@
 package bettergraves.api;
 
+import bettergraves.BetterGraves;
+import net.minecraft.util.crash.CrashException;
+import net.minecraft.util.crash.CrashReport;
+import org.apache.logging.log4j.Level;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,11 +23,17 @@ public class BetterGravesAPI {
     /**
      * Registers a DeathHandler and RestoreHandler pair with a String key to identify the pair.
      * @param key The identifier for this pair of handlers. Recommended to be your mod id.
-     * @param handler The DeathHandler that serializes your mod's drops into a Map between Integer and ItemStack
+     * @param deathHandler The DeathHandler that serializes your mod's drops into a Map between Integer and ItemStack
      * @param restoreHandler The RestoreHandler that deserializes the previously provided Map
      */
-    public void registerDeathHandler(String key, DeathHandler handler, RestoreHandler restoreHandler) {
-        deathHandlers.put(key, handler);
+    public static void registerDeathHandler(String key, DeathHandler deathHandler, RestoreHandler restoreHandler) {
+        BetterGraves.log(Level.INFO, "Registering handler " + key);
+        if (deathHandlers.containsKey(key)) {
+            throw new CrashException(
+                    CrashReport.create(new RuntimeException("Duplicate key"),
+                            "Duplicate DeathHandler key " + key));
+        }
+        deathHandlers.put(key, deathHandler);
         restoreHandlers.put(key, restoreHandler);
     }
 
