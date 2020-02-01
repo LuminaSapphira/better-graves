ef5ed56abd86a6e4a4848398868adc6b68e194e9
CerulanLumina <CerulanLumina@users.noreply.github.com>
2020-02-01 10:13:35 -0500

    Move from Overwrite to Redirect to enhance compatibility

--- src/main/java/bettergraves/mixin/MixinPlayerEntity.java
+++ src/main/java/bettergraves/mixin/MixinPlayerEntity.java
@@ -1,17 +1,29 @@
 package bettergraves.mixin;
 
+import bettergraves.BetterGraves;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(PlayerEntity.class)
-public class MixinServerPlayerEntity {
+public abstract class MixinPlayerEntity extends LivingEntity {
 
-    @Redirect(method = "dropInventory", at = @At(value = "INVOKE", ordinal = 4))
+    protected MixinPlayerEntity(EntityType<? extends LivingEntity> type, World world) {
+        super(type, world);
+    }
+
+    @Redirect(method = "dropInventory", at = @At(value = "INVOKE", ordinal = 4), require = 1)
     public void dropAll(PlayerInventory inventory) {
-        
+        if (this.world.isClient) return;
+        ServerPlayerEntity player = (ServerPlayerEntity)(Object)this;
+        BetterGraves.placeGrave(player.getBlockPos(), player, player.getServerWorld());
+        inventory.clear();
     }
 
 }
