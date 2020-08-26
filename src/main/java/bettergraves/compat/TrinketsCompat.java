2f78b99034515f84051b5349b647dae4b6cb101f
CerulanLumina <CerulanLumina@users.noreply.github.com>
2020-08-26 10:08:48 -0400

    Don't overwrite existing trinkets when restoring from grave. Fixes #9

--- src/main/java/bettergraves/compat/TrinketsCompat.java
+++ src/main/java/bettergraves/compat/TrinketsCompat.java
@@ -5,6 +5,7 @@ import com.google.common.collect.ImmutableMap;
 import dev.emi.trinkets.api.TrinketsApi;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
 
 public class TrinketsCompat {
 
@@ -18,9 +19,25 @@ public class TrinketsCompat {
             trinketsInv.clear();
             return map.build();
         }, (player, map) -> {
-            Inventory inv = TrinketsApi.getTrinketsInventory(player);
-            map.forEach(inv::setStack);
+            SetStackHelper inv = new SetStackHelper(TrinketsApi.getTrinketsInventory(player), player);
+            map.forEach(inv::setStackOrDrop);
         });
+    }
+
+    private static class SetStackHelper {
+        private final Inventory inv;
+        private final ServerPlayerEntity player;
+        public SetStackHelper(Inventory inv, ServerPlayerEntity player) {
+            this.inv = inv;
+            this.player = player;
+        }
+        public void setStackOrDrop(int i, ItemStack stack) {
+            if (inv.getStack(i).isEmpty()) {
+                inv.setStack(i, stack);
+            } else {
+                player.inventory.offerOrDrop(player.world, stack);
+            }
+        }
     }
 
 }
